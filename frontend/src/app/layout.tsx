8d9d1bc6a4ba7fd0230bd22bbc9d1ac760491c6a
Magomed Achtaew <122747757+magondev@users.noreply.github.com>
2024-06-08 23:27:08 +0200

    initial commit. created headerMenu for the client-frontend

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { createTheme, MantineProvider } from '@mantine/core'
+import { HeaderMenu } from "@/components/HeaderMenu/HeaderMenu";
+import '@mantine/core/styles.css';
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Mogham",
+  description: "Mogham - Description",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>)
+{
+  return (
+    <html lang="en">
+      <body>
+        <MantineProvider>
+          <HeaderMenu />
+          {children}
+        </MantineProvider>
+      </body>
+    </html>
+  );
+}
